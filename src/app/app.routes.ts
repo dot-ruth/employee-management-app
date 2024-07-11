0483e9f8a5ed4acd29930d7196b2547d03b42a37
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-11 11:44:46 +0300

    TASK|#2|added routing for the employee list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employees/components/employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employees/components/employee-detail/employee-detail.component';
+
+const routes: Routes = [
+  { path: 'employee', component: EmployeeListComponent },
+  { path: 'employee/create', component: EmployeeDetailComponent },
+  { path: 'employee/edit/:id', component: EmployeeDetailComponent },
+  { path: '', redirectTo: '/employee', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
