5f97f409ddb5ff0eac48d8c8dd760b9fea6484dd
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-11 11:38:13 +0300

    TASK|#2| added employee service for the CRUD functionality and an employee list view

--- src/app/employees/components/employee-list/employee-list.component.ts
+++ src/app/employees/components/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../../models/employee.model';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './employee-list.component.scss'
 })
 export class EmployeeListComponent {
+  public employees: Employee[] = [];
+  public pageSettings: Object;
 
+  constructor(private employeeService: EmployeeService, private router: Router) {
+    this.pageSettings = { pageSize: 10 };
+  }
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    this.employees = this.employeeService.getEmployees();
+  }
+
+  navigateToCreate(): void {
+    this.router.navigate(['/employee/create']);
+  }
+
+  editEmployee(employee: Employee): void {
+    this.router.navigate(['/employee/edit', employee.id]);
+  }
+
+  deleteEmployee(id: number): void {
+    this.employeeService.deleteEmployee(id);
+    this.loadEmployees();
+  }
 }
