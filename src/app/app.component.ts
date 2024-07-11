fc5248b7c1c7b4ce9a458d6fd44412b570c97b69
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-11 11:17:45 +0300

    TASK|NULL|install syncfusion and import necessary modules and componenets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EmployeesModule } from './employees/employees.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    EmployeesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
 
 @Component({
   selector: 'app-root',
