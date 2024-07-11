96bc952b37b111b13d2541269098ff313c9cd6dc
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-11 16:21:32 +0300

    TASK|#5|added search functionality

--- src/app/employees/components/employee-list/employee-list.component.ts
+++ src/app/employees/components/employee-list/employee-list.component.ts
@@ -5,6 +5,8 @@ import { EmployeeService } from '../../services/employee.service';
 import { GridModule } from '@syncfusion/ej2-angular-grids';
 import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { FormsModule} from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,13 +15,28 @@ import { DialogModule } from '@syncfusion/ej2-angular-popups';
     GridModule,
     DropDownListModule,
     DialogModule,
+    FormsModule,
+    CommonModule,
   ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.scss'
 })
+
 export class EmployeeListComponent implements OnInit {
   public employees: Employee[] = [];
   public pageSettings: Object;
+  public filteredEmployees: Employee[] = [];
+  public searchText = '';
+  public selectedSearchOption = '';
+
+  public searchOptions: { [key: string]: Object }[] = [
+    { id: 'name', name: 'Name' },
+    { id: 'email', name: 'Email' },
+    { id: 'department', name: 'Department' },
+    { id: 'position', name: 'Position' }
+  ];
+
+  public dropdownFields: Object = { text: 'name', value: 'id' };
 
   constructor(private employeeService: EmployeeService, private router: Router) {
     this.pageSettings = { pageSize: 10 };
@@ -45,4 +62,18 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.deleteEmployee(id);
     this.loadEmployees();
   }
+
+  onSearch(): void {
+    if (this.searchText.trim() === '') {
+      this.filteredEmployees = [...this.employees];
+    } else {
+      const searchTerm = this.searchText.toLowerCase();
+      const searchOption = this.selectedSearchOption as keyof Employee;
+      this.filteredEmployees = this.employees.filter(employee =>
+        `${employee[searchOption]}`.toLowerCase().includes(searchTerm)
+      );
+    }
+    console.log(this.filteredEmployees)
+  }
+
 }
